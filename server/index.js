f44e1ad8a6186bdcb295f960491e9bde69bc5883
Sajid Mahmud <54687706+sajidmahmud69@users.noreply.github.com>
2023-03-12 23:05:39 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,6 +9,22 @@ app.get('/', (req, res) => {
 	res.send('Api Home page')
 })
 
+app.get('/api/v1/', (req, res) => {
+	res.send('Api v1 page')
+})
+
+app.get('/api/v2/', (req, res) => {
+	res.send('Api v2 page')
+})
+
+app.post('/api/v3/', (req, res) => {
+	const { username, password }  = req.body;
+	if (username == 'Test' && password == 'test_password'){
+		res.send('Api v3 page')
+	}
+	else res.send('Unauthorized to access this page')
+})
+
 const server = app.listen (PORT, () => console.log(`Server running on port ${PORT}`))
 
 process.on("unhandledREjection", (err, promise) => {
